b43f4b5f9218e302103d3ef3a8d6a00ae3047dec
Валерия Алешина <leradavidova15@gmail.com>
2021-06-18 22:53:47 +0300

    front added on branch main

--- back/app.js
+++ back/app.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const config = require('config')
-const sequelize = require('./db/db')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const config = require("config");
+const sequelize = require("./db/db");
+const app = express();
+const cors = require("cors");
 const helmet = require("helmet");
-const auth = require('./controllers/auth.routes')
-const kat = require('./controllers/kat')
-const podkat = require('./controllers/podkat')
-const product = require('./controllers/product')
-const service = require('./controllers/service')
-const brand = require('./controllers/brand')
-const sotrud = require('./controllers/sotrud')
-const work = require('./controllers/work')
-const stock = require('./controllers/stock')
-const PORT = config.get('port') || 5000
-
+const auth = require("./controllers/auth.routes");
+const kat = require("./controllers/kat");
+const podkat = require("./controllers/podkat");
+const product = require("./controllers/product");
+const service = require("./controllers/service");
+const brand = require("./controllers/brand");
+const sotrud = require("./controllers/sotrud");
+const work = require("./controllers/work");
+const stock = require("./controllers/stock");
+const PORT = 4000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(helmet());
-app.use('/api/auth', auth)
-app.use('/api', kat)
-app.use('/api', product)
-app.use('/api', service)
-app.use('/api', brand)
-app.use('/api', sotrud)
-app.use('/api', stock)
-app.use('/api', podkat)
-app.use('/api', work)
-
+app.use("/api/auth", auth);
+app.use("/api", kat);
+app.use("/api", product);
+app.use("/api", service);
+app.use("/api", brand);
+app.use("/api", sotrud);
+app.use("/api", stock);
+app.use("/api", podkat);
+app.use("/api", work);
 
 async function start() {
-    try{   
-        await sequelize.authenticate();
-        await sequelize.sync();
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
-    } catch(e) {
-        console.log(e)
-        process.exit(1)
-    }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 }
-start()
+start();
--- back/seeders/20210603215151-demo-product.js
+++ back/seeders/20210603215151-demo-product.js
@@ -1,17 +1,22 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-
-      await queryInterface.bulkInsert('work', [{
-        date_time: new Date()
-        .toLocaleString()
-        .split(", ")
-        .join(" "),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
-
+    // await queryInterface.bulkInsert(
+    //   "product",
+    //   [
+    //     {
+    //       name_prod: "name",
+    //       price: 1234,
+    //       image: "jkfdbvkdbvkzdfbvdlzhvbfdlzh",
+    //       count: 123,
+    //       desc: "Описание данного товара достаточно велико, чтобы поместить сюда",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //   ],
+    //   {}
+    // );
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -21,5 +26,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
